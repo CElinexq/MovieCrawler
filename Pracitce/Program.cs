8377a4138e1875fcf17c22fd83e41b2ef9c94458
CElinexq <cocoxqzx@hotmail.com>
2023-01-19 17:20:57 +1100

    替换csv文件的逗号

--- Pracitce/Program.cs
+++ Pracitce/Program.cs
@@ -1,41 +1,22 @@
-﻿using HtmlAgilityPack;
-using System.Xml.Linq;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
+//用正则表达式取出时间和文件大小，将逗号替换成#@#，将原来有逗号的csv文件转换成#@#的csv文件
+string filePath = @"C:\Users\cocox\Desktop\BookCrawler\悦享云盘-Books 2023-01-16 13#00#30综合.csv";
+string[] lines = File.ReadAllLines(filePath);
+List<string> outputFile = new List<string>();
 
-
-
-string rootUrl = @"https://pity.eu.org/SP2/doc/%E6%BC%AB%E7%94%BB/500%E5%A5%97%E8%BF%9E%E7%8E%AF%E7%94%BB%E5%90%88%E9%9B%86/";
-var httpClient = new HttpClient();
-string html = await httpClient.GetStringAsync(rootUrl);
-var doc = new HtmlDocument();
-doc.LoadHtml(html);
-var pageNode = doc.DocumentNode.SelectSingleNode("//*[@id='nextpageform']");
-if (pageNode != null)
+foreach (var line in lines)
 {
-    //var pageLink = pageNode.Descendants("td").Last().InnerHtml;
-    var tdNode = pageNode.Descendants("td").ToArray()[1];
-    var pageNum= tdNode.ChildNodes.Where(x => x.NodeType == HtmlNodeType.Element).Count();
-    //foreach (var item in pageNum)
-    //{
-    //    Console.WriteLine(item.OuterHtml);
-    //}
-    
-    //Console.WriteLine(pageLink);
-    //Console.WriteLine(td2);
-    Console.WriteLine(pageNum);
-}
+    string patternDateTime = @"\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{1,2}:\d{1,2} [ap]m";
+    string patternSize = @"(\d+\.\d{1,2})\s(KB|MB|GB)";
 
+    Match matchDateTime = Regex.Match(line, patternDateTime);
+    string changeDateTime = line.Replace($",{matchDateTime},", $"#@#{matchDateTime}#@#");
 
-using (HttpClient client = new HttpClient())
-{
-    //获取第二页的html
-    List<KeyValuePair<string, string>> keyValuePairs = new List<KeyValuePair<string, string>>();
-    keyValuePairs.Add(new KeyValuePair<string, string>("pagenum", 3.ToString()));
-    FormUrlEncodedContent httpcontent = new FormUrlEncodedContent(keyValuePairs);
-    var response = await client.PostAsync(rootUrl, httpcontent);
-    string responseContent = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseContent);
-    //解析html文档
-    var doc1 = new HtmlDocument();
-    doc1.LoadHtml(responseContent);
+    Match matchSize = Regex.Match(line, patternSize);
+    string output = changeDateTime.Replace($"{matchSize},", $"{matchSize}#@#");
+    outputFile.Add(output);
+    Console.WriteLine(output);
 }
+File.AppendAllLines(@"C:\Users\cocox\Desktop\BookCrawler\悦享云盘-Books 2023-01-16 13#00#30综合01.csv", outputFile);
